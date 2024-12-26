0d3ae4feff61e05e917f051ea6bb9bd8545e0085
HarshitRajamanickam12 <rh30@ggnis.in>
2024-12-26 16:36:56 +0530

    Add files via upload
    This is a story generator made by me

---
+++ StoryGenerator.py
@@ -0,0 +1,8 @@
+import random
+when=["Yesterday","Last year","During my birthday 5 years ago"]
+who=[" i"," me and a stranger"," me and my brother"]
+went=[" my home"," usa"," russia"]
+With=[" my friend"," my family"," no one"]
+what=[" i found my lost dog"," i ate cake"," i was shivering in the cold"]
+
+print(random.choice(when)+random.choice(who)+" went to"+random.choice(went)+" with"+random.choice(With)+" and"+random.choice(what)+" there")
